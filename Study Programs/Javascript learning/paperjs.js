03b3fdeb89548684023e96ef018dd10f60face20
Adhitya Venkat <adhitya.venkat12@gmail.com>
2021-11-25 20:07:29 -0500

    commiting

--- Study Programs/Javascript learning/paperjs.js
+++ Study Programs/Javascript learning/paperjs.js
@@ -1,16 +1,32 @@
-var xrandom = Math.floor(Math.random() * 1000) + 1;
-var yrandom = Math.floor(Math.random() * 500) + 1;
+var xrandom = Math.floor(Math.random() * 900) + 100;
+var yrandom = Math.floor(Math.random() * 500) + 100;
 console.log(xrandom);
+
+var shape1 = new Shape.Circle(new Point(Math.floor(Math.random() * 900) + 100, Math.floor(Math.random() * 500) + 100), 125);
+shape1.fillColor = 'green';
+
+var shape4 = new Shape.Circle(new Point(Math.floor(Math.random() * 900) + 100, Math.floor(Math.random() * 500) + 100), 86);
+shape4.fillColor = 'red';
+
+var shape2 = new Shape.Circle(new Point(Math.floor(Math.random() * 900) + 100, Math.floor(Math.random() * 500) + 100), 40);
+shape2.fillColor = 'orange';
+
+
+var shape3 = new Shape.Circle(new Point(Math.floor(Math.random() * 900) + 100, Math.floor(Math.random() * 500) + 100), 38);
+shape3.fillColor = 'blue';
+
+var shape0 = new Shape.Circle(new Point(Math.floor(Math.random() * 900) + 100, Math.floor(Math.random() * 500) + 100), 25);
+shape0.fillColor = 'yellow';
+
 var shape = new Shape.Circle(new Point(xrandom, yrandom), 100);
 shape.fillColor = 'black';
 
-var text = new PointText(new Point(xrandom - 70, yrandom));
+var text = new PointText(new Point(xrandom - 70, yrandom + 9));
 text.fillColor = 'white';
 text.fontSize = "25";
 text.fontFamily = "Trebuchet MS";
 text.content = 'Hello, World!';
 
-var rectangle = new Rectangle(new Point(50, 50), new Point(150, 100));
-var path = new Path.Rectangle(rectangle);
-path.fillColor = 'white';
-path.selected = true;
+setInterval(function () {
+    location.reload();
+}, 10000);
