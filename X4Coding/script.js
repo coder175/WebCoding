5cfe93dfe6923d03eaff577b471986f26cb5eb8e
Adhitya Venkat <adhitya.venkat12@gmail.com>
2022-03-07 21:01:38 -0500

    updated editor

--- X4Coding/script.js
+++ X4Coding/script.js
@@ -10,7 +10,7 @@ document.querySelector('.Menu').addEventListener('click', () => {
 });
 
 var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-    lineNumbers: false,
+    lineNumbers: true,
     matchBrackets: true,
     autoCloseBrackets: true,
     autoCloseTags: true,
@@ -18,27 +18,59 @@ var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
 });
 
 var editor = CodeMirror.fromTextArea(document.getElementById("code1"), {
-    lineNumbers: false,
+    lineNumbers: true,
     matchBrackets: true,
     autoCloseBrackets: true,
     autoCloseTags: true,
     mode: "htmlmixed",
 });
 var editor = CodeMirror.fromTextArea(document.getElementById("code2"), {
-    lineNumbers: false,
+    lineNumbers: true,
     matchBrackets: true,
     autoCloseBrackets: true,
     autoCloseTags: true,
     mode: "htmlmixed",
 });
 function run() {
-    html_value = editor.getValue();
-    doc.open();
-    doc.write(html_value);
-    doc.close();
-}
+    var startingFirst = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>`
+var startingFirstMiddle = `
+</style>
+</head>
+<body>`
+var startingMiddle = `
+<script>`
+var startingLast = `
+</script>
+</body>
+</html>`
 
-function html() {
+var bodyText = document.getElementById('code').innerText;
+var headText = document.getElementById('code1').innerText;
+var scriptText = document.getElementById('code2').innerText;
+var end = startingFirst + headText + startingFirstMiddle + bodyText + startingMiddle + scriptText + startingLast;
+console.log(end);
+}
+run();
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.transform = "translate(-99999px, 0px)";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.transform = "translate(0px, 0px)";
+    evt.currentTarget.className += " active";
+  }
+
+  function html() {
 }
 
 function css() {
