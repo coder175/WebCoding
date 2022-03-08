3255c00dee5dd98a6eb2f8a65171b8372436f316
Adhitya Venkat <adhitya.venkat12@gmail.com>
2022-03-08 17:37:42 -0500

    saving

--- Learn/script.js
+++ Learn/script.js
@@ -3,10 +3,8 @@ document.querySelector('.Menu').addEventListener('click', () => {
     if (check == 1) {
         check++;
         document.querySelectorAll('.navItem').forEach(display => display.classList.add('displayBlock'));
-        document.querySelectorAll('.navItem').forEach(display => display.classList.remove('displayNone'));
     } else {
         check--;
         document.querySelectorAll('.navItem').forEach(display => display.classList.remove('displayBlock'));
-        document.querySelectorAll('.navItem').forEach(display => display.classList.add('displayNone'));
     }
 });
