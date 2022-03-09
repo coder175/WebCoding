0154497eefae99931d534dcf9342619a4edfeae6
Adhitya Venkat <adhitya.venkat12@gmail.com>
2022-03-09 17:28:43 -0500

    saving

--- Learn/script.js
+++ Learn/script.js
@@ -1,10 +1,20 @@
-var check = 1;
+var Menucheck = 1;
 document.querySelector('.Menu').addEventListener('click', () => {
-    if (check == 1) {
-        check++;
+    if (Menucheck == 1) {
+        Menucheck++;
         document.querySelectorAll('.navItem').forEach(display => display.classList.add('displayBlock'));
     } else {
-        check--;
+        Menucheck--;
         document.querySelectorAll('.navItem').forEach(display => display.classList.remove('displayBlock'));
     }
 });
+var courseCheck = 0;
+function courses() {
+    if (courseCheck == 0) {
+        document.getElementById('drop1').style.display = "flex";
+        courseCheck++;
+    } else {
+        document.getElementById('drop1').style.display = "none";
+        courseCheck--;
+    }
+}
