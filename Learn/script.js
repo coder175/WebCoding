9cd08ca721b459410613d4d4376f33db026cd3eb
Adhitya Venkat <adhitya.venkat12@gmail.com>
2022-03-17 20:47:30 -0400

    updated navbar

--- Learn/script.js
+++ Learn/script.js
@@ -8,16 +8,18 @@ document.querySelector('.Menu').addEventListener('click', () => {
         document.querySelectorAll('.navItem').forEach(display => display.classList.remove('displayBlock'));
     }
 });
-var courseCheck = 0;
-function courses() {
-    if (courseCheck == 0) {
-        document.getElementById('drop1').style.display = "flex";
-        courseCheck++;
-        document.getElementById('arrow1').style.transform = "rotate(180deg)";
-    } else {
-        document.getElementById('drop1').style.display = "none";
-        courseCheck--;
-        document.getElementById('arrow1').style.transform = "rotate(0deg)";
 
+var checkChildLocation = '';
+function drop(childLocation) {
+    if (childLocation == checkChildLocation) {
+        document.querySelectorAll('.drop').forEach(a => { a.style.display = 'none'});
+        document.querySelectorAll('.arrow').forEach(e => { e.style.transform = 'rotate(0deg)'});
+        checkChildLocation = '';
+    } else {
+        document.querySelectorAll('.drop').forEach(d => { d.style.display = 'none'});
+        document.querySelector(`.drop:nth-child(${childLocation})`).style.display = 'flex';
+        document.querySelectorAll('.arrow').forEach(e => { e.style.transform = 'rotate(0deg)'});
+        document.querySelector(`#arrow${childLocation}`).style.transform = 'rotate(180deg)';
+        checkChildLocation = childLocation;
     }
 }
