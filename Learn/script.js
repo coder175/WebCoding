fb4974f15a69fb6433d08a147d9310a6a2441ea6
Adhitya Venkat <adhitya.venkat12@gmail.com>
2022-04-09 20:05:58 -0400

    made typewriter

--- Learn/script.js
+++ Learn/script.js
@@ -12,13 +12,13 @@ document.querySelector('.Menu').addEventListener('click', () => {
 var checkChildLocation = '';
 function drop(childLocation) {
     if (childLocation == checkChildLocation) {
-        document.querySelectorAll('.drop').forEach(a => { a.style.display = 'none'});
-        document.querySelectorAll('.arrow').forEach(e => { e.style.transform = 'rotate(0deg)'});
+        document.querySelectorAll('.drop').forEach(a => { a.style.display = 'none' });
+        document.querySelectorAll('.arrow').forEach(e => { e.style.transform = 'rotate(0deg)' });
         checkChildLocation = '';
     } else {
-        document.querySelectorAll('.drop').forEach(d => { d.style.display = 'none'});
+        document.querySelectorAll('.drop').forEach(d => { d.style.display = 'none' });
         document.querySelector(`.drop:nth-child(${childLocation})`).style.display = 'flex';
-        document.querySelectorAll('.arrow').forEach(e => { e.style.transform = 'rotate(0deg)'});
+        document.querySelectorAll('.arrow').forEach(e => { e.style.transform = 'rotate(0deg)' });
         document.querySelector(`#arrow${childLocation}`).style.transform = 'rotate(180deg)';
         checkChildLocation = childLocation;
     }
@@ -32,3 +32,43 @@ function IDtoogleDisplay(id) {
         document.getElementById(id).style.display = "block";
     }
 }
+
+const words = ["HTML", "CSS", "JavaScript"];
+let i = 0;
+let timer;
+
+function typingEffect() {
+    let word = words[i].split("");
+    var loopTyping = function () {
+        if (word.length > 0) {
+            document.getElementById('word').innerHTML += word.shift();
+        } else {
+            setTimeout(deletingEffect, 1000);
+            return false;
+        };
+        timer = setTimeout(loopTyping, 130);
+    };
+    loopTyping();
+};
+
+function deletingEffect() {
+    let word = words[i].split("");
+    var loopDeleting = function () {
+        if (word.length > 0) {
+            word.pop();
+            document.getElementById('word').innerHTML = word.join("");
+        } else {
+            if (words.length > (i + 1)) {
+                i++;
+            } else {
+                i = 0;
+            };
+            typingEffect();
+            return false;
+        };
+        timer = setTimeout(loopDeleting, 70);
+    };
+    loopDeleting();
+};
+
+typingEffect();
