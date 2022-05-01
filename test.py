662e674ce53912b632573f5d168b77e8d603c20f
Adhitya Venkat <adhitya.venkat12@gmail.com>
2022-05-01 16:54:40 -0400

    made adding program

--- test.py
+++ test.py
@@ -1,7 +1,4 @@
-operation = input('What operation do you want to use? Type in Add, Sub, Multiply, or Divide. ')
-operation = operation.lower()
-while operation == 'add' or operation == 'sub' or operation == 'multiply' or operation == 'divide':
-    print('Yes!')
+
 
 number1 = input('Enter Your first number ')
 while str.isalpha(number1):
@@ -11,5 +8,5 @@ while str.isalpha(number1):
 number2 = input('Enter Your second number ')
 while str.isalpha(number2):
     print('The Number you have entered in not Numeric. Please enter a valid number.')
-    number2 = input('Enter Your First Valid Number ')
+    number2 = input('Enter Your Second Valid Number ')
 print(int(number1) + int(number2))
