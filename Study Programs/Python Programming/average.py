e62f2ed06fc6fbbd456d9333eef68a2f13ab38f7
Adhitya Venkat <adhitya.venkat12@gmail.com>
2022-09-13 19:52:51 -0400

    Move files into different repo

--- Study Programs/Python Programming/average.py
+++
@@ -1,8 +0,0 @@
-numbers = input('What are your numbers? Seperate them with a comma. ')
-numbers = numbers.split(',')
-numbers = [int(y) for y in numbers]
-i = 0
-for x in numbers:
-    i = i + x
-i = i / len(numbers)
-print(i)
