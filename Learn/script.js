799135c51f45de1dbc943fcd4e614b8a474388fe
Green <87390224+coder175@users.noreply.github.com>
2022-11-15 20:05:46 -0500

    continue working on making typewriter

--- Learn/script.js
+++ Learn/script.js
@@ -1,66 +1,28 @@
-phrases = ['To code from scrach', 'To do math', 'To be smart', 'test1', 'test2', 'test3']
-round = 0
+const phrases = ['To be smarter (duh.)']
+let round = 0
 function type() {
-    phrs = phrases[round].split('');
-    console.log(phrs.length)
+    let phrs = phrases[round].split('');
     for (let x = 0; x < phrs.length; x++) {
         setTimeout(() => {
             document.getElementById('subtitle').innerHTML += phrs.shift();
         }, x * 200);
     }
-    setTimeout(() => { del() }, 1500);
+    setTimeout(() => {
+        del()
+    }, (phrs.length*200)+1500);
 }
 function del() {
-    phrsLen = phrases[round].split('');
+    let phrsLen = phrases[round].split('');
     for (let x = 0; x < phrsLen; x++) {
+        alert('dodo')
         setTimeout(() => {
             document.getElementById('subtitle').innerText.slice(0, -1);
-        }, x * 50);
+        }, x * 1000);
     }
     if (phrases.length > (round + 1)) round++
     else round = 0
     setTimeout(() => { type() }, 500);
 }
+function blink() {
+}
 type()
-
-
-
-// const words = ["CS  S3.", "HTML5.", "javascript."];
-// let i = 0;
-// let timer;
-
-// function typingEffect() {
-// 	let word = words[i].split("");
-// 	var loopTyping = function() {
-// 		if (word.length > 0) {
-// 			document.getElementById('word').innerHTML += word.shift();
-// 		} else {
-// 			deletingEffect();
-// 			return false;
-// 		};
-// 		timer = setTimeout(loopTyping, 100);
-// 	};
-// 	loopTyping();
-// };
-
-// function deletingEffect() {
-// 	let word = words[i].split("");	
-// 	var loopDeleting = function() {
-// 		if (word.length > 0) {
-// 			word.pop();
-// 			document.getElementById('word').innerHTML = word.join("");
-// 		} else {
-// 			if (words.length > (i + 1)) {
-// 				i++;
-// 			} else {
-// 				i = 0;
-// 			};
-// 			typingEffect();
-// 			return false;
-// 		};
-// 		timer = setTimeout(loopDeleting, 50);
-// 	};
-// 	loopDeleting();
-// };
-
-// typingEffect();
