c7528caa75401a499728af6da3098dbf441c7c8e
Adhitya Venkat <adhitya.venkat12@gmail.com>
2022-11-16 20:34:28 -0500

    Finished Typewriter, creating navbar, finished gradient slide, and centered text

--- Learn/script.js
+++ Learn/script.js
@@ -1,28 +1,47 @@
-const phrases = ['To be smarter (duh.)']
+const phrases = ['How the first printing press became','Other stuff']
 let round = 0
 function type() {
+    document.getElementById('cursor').style.opacity = '1'
     let phrs = phrases[round].split('');
     for (let x = 0; x < phrs.length; x++) {
         setTimeout(() => {
-            document.getElementById('subtitle').innerHTML += phrs.shift();
+            document.querySelector('#subtitle').innerHTML += phrs.shift();
         }, x * 200);
     }
     setTimeout(() => {
-        del()
-    }, (phrs.length*200)+1500);
+        blink(8)
+        setTimeout(() => { del() }, 4500);
+    }, phrs.length * 200);
 }
 function del() {
+    document.getElementById('cursor').style.opacity = '1'
     let phrsLen = phrases[round].split('');
-    for (let x = 0; x < phrsLen; x++) {
-        alert('dodo')
+    for (let x = 0; x < phrsLen.length; x++) {
         setTimeout(() => {
-            document.getElementById('subtitle').innerText.slice(0, -1);
-        }, x * 1000);
+            document.querySelector('#subtitle').innerHTML = phrsLen.slice(0, -(x + 1)).join('');
+        }, x * 100);
     }
     if (phrases.length > (round + 1)) round++
     else round = 0
-    setTimeout(() => { type() }, 500);
+    setTimeout(() => {
+        blink(4)
+        setTimeout(() => { type() }, 2500);
+    }, phrsLen.length * 100);
 }
-function blink() {
+let rtime = 0
+function blink(stime) {
+    let c = document.getElementById('cursor');
+    let interval = setInterval(function () {
+        if (rtime >= stime) {
+            rtime = 0
+            clearInterval(interval);
+        }
+        rtime++;
+        c.style.opacity = (c.style.opacity == '0' ? '1' : '0');
+    }, 500);
 }
-type()
+setTimeout(() => {
+    type()
+    document.getElementById('restOfBody').style.visibility = 'visible';
+    document.getElementById('restOfBody').style.opacity = '1';
+}, 1500);
