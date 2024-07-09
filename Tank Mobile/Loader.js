5b06cb3a3fcb41f2d6d536433e0b1176fa4a3a3f
Adhitya Venkat <adhitya.venkat12@gmail.com>
2024-07-09 11:09:03 -0400

    Changing Intro Screen

--- Tank Mobile/Loader.js
+++ Tank Mobile/Loader.js
@@ -11,8 +11,10 @@ document.addEventListener('DOMContentLoaded', function () {
         {type: 'script', src: 'https://unpkg.com/swiper/swiper-bundle.min.js', size: 6},
     ];
 
-    const loadingScreen = document.getElementById('loadingAnimation');
+    const loadingScreen = document.getElementById('intro');
+    const loadingScreenContainer = document.getElementById('container')
     const content = document.getElementById('restOfContent');
+    const title = document.getElementById('title')
     const progressBar = document.getElementById('loadingProgressBarStartup');
     let totalSize = resources.reduce((acc, resource) => acc + (resource.size || 0), 0);
     let loadedSize = 0;
@@ -23,7 +25,13 @@ document.addEventListener('DOMContentLoaded', function () {
         progressBar.style.width = progress + '%';
         if (progress >= 100) {
             setTimeout(() => {
-                loadingScreen.style.display = 'none';
+                loadingScreenContainer.innerHTML = `
+                <button class="start" id="start">
+    				<span class="material-symbols-outlined">keyboard_double_arrow_left</span>
+    				PLAY
+    				<span class="material-symbols-outlined">keyboard_double_arrow_right</span>
+                </button>`
+                loadingScreenContainer.classList.add('nowButton')
                 content.style.display = 'block';
                 s();
             }, 500); // Small delay to ensure the progress bar completes its animation
@@ -57,6 +65,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 await loadResource(resource);
             } catch (error) {
                 console.error(`Failed to load resource: ${resource.href || resource.src}`, error);
+                title.innerHTML = `<span>NO</span> <span>INTERNET</span>`
+                progressBar.style.backgroundColor = "red"
             }
         }
     }
